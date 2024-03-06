d7f9e2e0265d4fc5d7b6eb911ca87ac610eb3ad9
DictumMortuum <theio.vrefos@gmail.com>
2024-03-06 21:30:14 +0200

    [v0.1.8] added teams manipulation on the play edit/creation screen

--- src/resources/plays.js
+++ src/resources/plays.js
@@ -19,17 +19,38 @@ import {
   DateInput,
   Edit,
   ReferenceField,
-  TextInput,
   BooleanInput,
+  TextInput,
   FunctionField
 } from 'react-admin';
 
 const createTransform = record => {
-  return {
-    ...record,
-    date: new Date(record.date),
-    stats: record.stats.map(d => ({...d, boardgame_id: record.boardgame_id})),
-    play_data: record.play_data === "" ? null : record.play_data,
+  if (record.play_data.teams.length === 0) {
+    delete record.play_data.teams;
+
+    return {
+      ...record,
+      date: new Date(record.date),
+      stats: record.stats.map(d => ({...d, boardgame_id: record.boardgame_id})),
+      play_data: record.play_data === "" ? null : record.play_data,
+    }
+  } else {
+    const teams = record.play_data.teams.map(d => {
+      if (typeof d === "string") {
+        return d.split(",").map(i => parseInt(i))
+      } else if (Array.isArray(d)) {
+        return d
+      }
+
+      return [];
+    });
+
+    return {
+      ...record,
+      date: new Date(record.date),
+      stats: record.stats.map(d => ({...d, boardgame_id: record.boardgame_id})),
+      play_data: record.play_data === "" ? null : { ...record.play_data, teams },
+    }
   }
 }
 
@@ -41,7 +62,8 @@ const PlayForm = () => {
       </ReferenceInput>
       <LocationInput />
       <DateInput source="date" />
-      <TextInput source="play_data" />
+      {/* <TextInput source="play_data" /> */}
+      <BooleanInput source="play_data.solo" />
       <ArrayInput source="stats">
         <SimpleFormIterator inline>
           <TextField source="player.id" />
@@ -50,6 +72,11 @@ const PlayForm = () => {
           </ReferenceInput>
           <NumberInput source="data.score" />
           <BooleanInput source="data.won" />
+        </SimpleFormIterator>
+      </ArrayInput>
+      <ArrayInput source="play_data.teams">
+        <SimpleFormIterator inline>
+          <TextInput />
         </SimpleFormIterator>
       </ArrayInput>
     </SimpleForm>
